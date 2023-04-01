2725ed271abb928c37e1d3e3d797746491496bdd
Felix <felixchh123@gmail.com>
2023-04-01 18:03:26 +0000

    classes

--- src/main/java/com/example/traficmanager/Controller/AirlineController.java
+++ src/main/java/com/example/traficmanager/Controller/AirlineController.java
@@ -1,5 +1,11 @@
 package com.example.traficmanager.Controller;
 
+import com.example.traficmanager.model.AirPlane;
+
 public class AirlineController {
+    public static double calculeTemps(AirPlane airPlane){
+        return 0;
+    }
+    
 
 }
